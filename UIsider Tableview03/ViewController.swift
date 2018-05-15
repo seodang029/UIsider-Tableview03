2d86302dce56be69c616571f07e572ba5658c195
csd5766 <csd5766@naver.com>
2018-05-15 15:30:56 +0900

    슬라이드 테이블

--- UIsider Tableview03/ViewController.swift
+++ UIsider Tableview03/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var myslider: UISlider!
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var tbv: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func slider(_ sender: Any) {
+        tbv.dataSource = self
+        tbv.delegate = self
+    
+        
+        let val = Int(myslider.value)
+        print(val)
+        print("슬라이드 움직임")
+        lbl.text = String(val)
     }
-
-
+    //필수적인 delegate 함수 구현
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = tbv.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = String(Int(myslider.value) + indexPath.row)
+        return cell
+    }
+            
+            
 }
+    
+
+
+
 
